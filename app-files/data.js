44c741d9bd3b639f0cd118588675db2c959d33bd
Guilherme Hammel <hammel.cattaneo@unesp.br>
2021-11-22 11:20:20 -0300

    new files
    more

---
+++ app-files/data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-redo",
+      "name": "1-redo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.11545963323128206,
+        "pitch": 0.1470383558944537,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05648811471807136,
+          "pitch": 0.27576340871072347,
+          "rotation": 0,
+          "target": "1-2-redo"
+        },
+        {
+          "yaw": 1.8205235891301346,
+          "pitch": 0.36024719181784093,
+          "rotation": 12.566370614359176,
+          "target": "3-4-redo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7979507616540165,
+          "pitch": 0.07981677208376681,
+          "title": "<h1 class=\"page-header__title\" id=\"firstHeading\" style=\"box-sizing: inherit; border: 0px; vertical-align: baseline; font-size: 36px; font-weight: 300; letter-spacing: 0.25px; line-height: 1.25; overflow-wrap: break-word; word-break: break-word; color: rgb(230, 230, 230); font-family: rubik, helvetica, arial, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; background-color: rgb(0, 0, 0);\">Body:</h1>",
+          "text": "<h1 class=\"page-header__title\" id=\"firstHeading\" style=\"box-sizing: inherit; border: 0px; vertical-align: baseline; line-height: 1.25; overflow-wrap: break-word; word-break: break-word; background-color: rgb(0, 0, 0);\"><font color=\"#e6e6e6\" face=\"rubik, helvetica, arial, sans-serif\"><span style=\"font-size: 36px; letter-spacing: 0.25px;\">Harry Mason</span></font><br></h1>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-redo",
+      "name": "2-redo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1348506844456132,
+          "pitch": 0.25543777669177636,
+          "rotation": 0,
+          "target": "0-1-redo"
+        },
+        {
+          "yaw": 2.2727758113152348,
+          "pitch": 0.2998352060288454,
+          "rotation": 0,
+          "target": "2-3-redo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-redo",
+      "name": "3-redo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24166726126405536,
+          "pitch": 0.23286339380722154,
+          "rotation": 0,
+          "target": "1-2-redo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4-redo",
+      "name": "4-redo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1419988683975841,
+          "pitch": 0.38556553110897696,
+          "rotation": 0,
+          "target": "0-1-redo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Silent Hill 3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
